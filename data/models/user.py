0f62aff10ee27f4208ac33fb77ce49dea1e3b024
Montorz <glebsankov30@mail.ru>
2024-09-22 21:28:41 +0700

    add register

---
+++ data/models/user.py
@@ -0,0 +1,9 @@
+import sqlalchemy
+from data.db_session import SqlAlchemyBase
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from data import db_session
+from flask_cors import CORS
+from data.models.user import User
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/register": {"origins": "http://127.0.0.1:5500"}})
+db_session.global_init('db/casino.db')
+
+@app.route('/register', methods=['POST', 'OPTIONS'])
+def register():
+    if request.method == 'OPTIONS':
+        return '', 200
+
+    data = request.get_json()
+    user =  User(
+        name=data['login'],
+        password=data['password'],
+    )
+
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    return jsonify({"status": "success", "register": data}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
