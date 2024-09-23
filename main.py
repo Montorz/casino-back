0139e2a16599da38d5495bb640ba5d9ac0597096
Montorz <glebsankov30@mail.ru>
2024-09-23 21:02:16 +0700

    add login

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 from data import db_session
 from flask_cors import CORS
 from data.models.user import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
@@ -33,6 +33,22 @@ def register():
     db_sess.commit()
 
     return jsonify({"status": "success", "register": data}), 200
+
+@app.route('/login', methods=['POST', 'OPTIONS', 'GET'])
+def login():
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'ok'}), 200
+
+    data = request.get_json()
+    db_sess = db_session.create_session()
+
+    # Проверка пользователя на правильность ввода пароля
+    if db_sess.query(User).filter(check_password_hash(User.hashed_password, data['password'])).first():
+        return jsonify({"status": "error", "message": "Пользователь с таким логином уже существует"}), 400
+
+
+    return jsonify({"status": "success", "login": data}), 200
+
 
 
 if __name__ == "__main__":
