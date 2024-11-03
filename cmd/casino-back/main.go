e49e78ac3c3307eacfbfd350ac705843507b77fa
Montorz <glebsankov30@mail.ru>
2024-11-03 16:20:44 +0700

    add corsConfig

--- cmd/casino-back/main.go
+++ cmd/casino-back/main.go
@@ -30,7 +30,14 @@ func main() {
 	authHandler := handler.NewAuthHandler(authService)
 
 	r := gin.New()
-	r.Use(cors.Default())
+	corsConfig := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+	})
+
+	r.Use(corsConfig)
 
 	auth := r.Group("/auth")
 	{
